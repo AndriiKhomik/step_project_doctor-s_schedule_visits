3e2b07160799fe76f2e4a9b7fee0c8a98ab0bf31
AndriiKhomik <satvaer@gmail.com>
2021-09-13 16:23:50 +0300

    refactor class Element

--- src/components/element/element.js
+++ src/components/element/element.js
@@ -4,7 +4,7 @@ class Element {
   }
   createElement(tagName, classNames = [], text = '') {
     const element = document.createElement(tagName);
-    if (text) { element.textContent = text; }
+    element.textContent = text;
     element.classList.add(...classNames);
     return element
   }
@@ -16,12 +16,10 @@ class Element {
 
   createContainer() {
     this.containerEl = this.createElement('div', ['container']);
-    console.log(this.containerEl)
     document.querySelector('#root').append(this.containerEl);
     return this.containerEl
   }
 }
-
 
 new Element();
 
