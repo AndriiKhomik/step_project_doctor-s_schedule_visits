5e72ce4204010214a17bcaa93cdf3015ca9e6297
AndriiKhomik <satvaer@gmail.com>
2021-09-13 16:11:46 +0300

    add createContainer and addAttribute methods to class Element

--- src/components/element/element.js
+++ src/components/element/element.js
@@ -1,10 +1,27 @@
 class Element {
+  constructor() {
+    this.createContainer()
+  }
   createElement(tagName, classNames = [], text = '') {
     const element = document.createElement(tagName);
     if (text) { element.textContent = text; }
     element.classList.add(...classNames);
     return element
   }
+
+  addAttribute(element, attributeName, attributeValue) {
+    element.setAttribute(attributeName, attributeValue);
+    return element
+  }
+
+  createContainer() {
+    this.containerEl = this.createElement('div', ['container']);
+    console.log(this.containerEl)
+    document.querySelector('#root').append(this.containerEl);
+    return this.containerEl
+  }
 }
+
+new Element();
 
 export default Element
