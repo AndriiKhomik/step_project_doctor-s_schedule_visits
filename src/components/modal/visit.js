3c3b0ff14b646085659adf3dd572ec2e559c6615
Olena Savchenko <olsavchenko1@gmail.com>
2021-09-15 17:48:08 +0300

    format visit form

--- src/components/modal/visit.js
+++ src/components/modal/visit.js
@@ -1,27 +1,37 @@
 class Visit {
+    constructor() {
+
+    }
     rendeInputFields() {
-        const html = `<label for="fullName" class="form-label"
-        >Enter your name:
-    </label>
+        const html = `<label for="fullName" class="form-label">Enter your name:
+        </label>
     <input required type="text" name="fullName"/>
-        <label for="target" class="form-label">Target of visit:</label>
+        <label for="target" class="form-label">Target of visit:
+        </label>
         <input required type="text" name="target"/>
-        <label for="description" class="form-label"
-            >Brief description of the visit:</label>
+        <label for="description" class="form-label">Brief description of the visit:
+        </label>
         <input type="text" name="description"/>
-        <label for="urgency" class="form-label"
-            >Choose urgency of the visit:
+        <label for="urgency" class="form-label">Choose urgency of the visit:
         </label>
         <select required name="urgency" class="visit-form-select form-select">
             <option value="ordinary">Ordinary</option>
             <option value="priority">Priority</option>
             <option value="urgent">Urgent</option>
         </select>
-        <label for="date-visit" class="form-label"> Date of visit: </label>
+        <label for="date-visit" class="form-label"> Date of visit:
+        </label>
        <input required type="datetime-local" name="date-visit"`
         return html
     }
 
+    getValue() {
+        console.log();
+    }
+
+    getContent() {
+
+    }
 }
 
 export default Visit
