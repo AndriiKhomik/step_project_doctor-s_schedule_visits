b61d93aa69f85582ccefe76c8de70cfda0230fc5
Olena Savchenko <olsavchenko1@gmail.com>
2021-09-21 23:55:48 +0300

    add validation msg in visitModal

--- src/components/modal/validateModal.js
+++ src/components/modal/validateModal.js
@@ -1,3 +1,24 @@
+import * as PNotify from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css"
+import "@pnotify/core/dist/BrightTheme.css"
+
+function showStackBottomLeft(textValue) {
+    if (typeof window.stackBottomLeft === 'undefined') {
+        window.stackBottomLeft = new PNotify.Stack({
+            dir1: 'right',
+            dir2: 'up',
+            firstpos1: 25,
+            firstpos2: 25,
+            push: 'top',
+            maxStrategy: 'close'
+        });
+    }
+
+    PNotify.info({
+        text: textValue,
+        stack: window.stackBottomLeft
+    });
+}
 
 export const getDateTimeString = () => {
     const now = new Date()
@@ -22,6 +43,21 @@ export const checkLoginValue = (el, data) => {
 export const checkVisitValue = (el, minValue, maxValue) => {
     if (+el.value < minValue || +el.value > maxValue || el.value === "") {
         el.style.borderColor = "red"
+        let field = null
+        switch (el.name) {
+            case "age":
+                field = "Age"
+                break;
+            case "body-mass-index":
+                field = "Body mass index"
+                break;
+            case "pressure":
+                field = "Normal pressure"
+                break;
+            default:
+                break;
+        }
+        showStackBottomLeft(`Check field "${field}"  (should be number from ${minValue} to ${maxValue})`)
         return ''
     }
     else {
